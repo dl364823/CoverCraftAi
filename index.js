3b95e0aad099b770c36f296cbdf103b5bf457f14
dl364823 <146682379+dl364823@users.noreply.github.com>
2024-12-15 21:25:00 -0500

    fixed the cors

--- index.js
+++ index.js
@@ -12,7 +12,10 @@ const app = express();
 const upload = multer();
 
 const corsOptions = {
-    origin: ['https://cover-craft-ai-8yao.vercel.app', 'http://localhost:3000'], 
+    origin: [
+        'https://cover-craft-ai-8yao-qnna6xvh2-dl364823s-projects.vercel.app',
+        'https://cover-craft-ai-8yao.vercel.app', 
+        'http://localhost:3000'], 
     methods: ['GET', 'POST'],     
     allowedHeaders: ['Content-Type'],                     
     credentials: true                                 
